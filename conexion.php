543fc1500a2fa158916e0dcfa1f63f040d14497f
AlexTorresGomez <106146643+AlexTorresGomez@users.noreply.github.com>
2025-04-09 18:37:32 +0200

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,13 +1,16 @@
 <?php
-$host = "TU_HOST.aivencloud.com";
-$port = 3306;
-$dbname = "TU_BASE";
-$user = "TU_USUARIO";
-$password = "TU_PASSWORD";
+$host = "mysql-cluster-role-alextorresgomez47-b004.i.aivencloud.com";
+$port = 11439;
+$dbname = "defaultdb";  // Asegúrate de usar el nombre correcto de tu base de datos
+$user = "avnadmin";
+$password = "AVNS_tCVtikVVgXH9mp8Rb1F";
 
+// Crear la conexión
 $conn = new mysqli($host, $user, $password, $dbname, $port);
 
+// Comprobar si la conexión fue exitosa
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 ?>
+
