db7607ce8f40a749d2b36ed09db69e4bd8d35c46
2002joel <99648659+2002joel@users.noreply.github.com>
2025-04-09 17:51:03 +0200

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,9 @@
 <?php
-
+$host = 'mysql-cluster-role-alextorresgomez47-b004.i.aivencloud.com';
+$port = 11439;
+$dbname = 'defaultdb';
+$user = 'avnadmin';
+$password = 'AVNS_Tm7Y1J05zgl0T4HSbtI';
 
 try {
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;sslmode=require", $user, $password);
